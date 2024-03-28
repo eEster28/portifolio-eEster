a6322ca9763e978bdd6315c5a54ce12e6cafb9c1
Ester Moura <135028149+eEster28@users.noreply.github.com>
2024-03-28 10:00:40 -0300

    feat: adcicionando section about me

---
+++ src/components/Sections/SectionAboutMe/index.jsx
@@ -0,0 +1,12 @@
+import Styles from "./style.module.scss"
+
+export const SectionAboutMe = () => {
+    return(
+        <section className={Styles.conteiner}>
+            <h2 className="title two sub">Sobre mim</h2>
+            <p className="title paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam quibusdam incidunt consequuntur enim quasi? Ipsam rerum quae amet nesciunt, natus facilis delectus tenetur, unde doloremque veritatis ducimus iusto ullam at.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam quibusdam incidunt consequuntur enim quasi? Ipsam rerum quae amet nesciunt, natus facilis delectus tenetur, unde doloremque veritatis ducimus iusto ullam at.
+            </p>
+        </section>
+    )
+}
--- src/pages/DashboardPage/index.jsx
+++ src/pages/DashboardPage/index.jsx
@@ -1,18 +1,23 @@
 import { useContext } from "react"
+import Styles from "./style.module.scss"
 import { Header } from "../../components/Header"
 import { HeaderContext } from "../../providers/HeaderContext"
 import { SectionTitle } from "../../components/Sections/SectionTitle"
 import { SectionPhoto } from "../../components/Sections/SectionPhoto"
+import { SectionAboutMe } from "../../components/Sections/SectionAboutMe"
 
 export const DeshboardPage = () => {
     const { theme } = useContext(HeaderContext)
 
     return (
         <body data-theme={theme}>
-            <Header/>
+            <Header />
             <main>
-                <SectionTitle/>
-                <SectionPhoto/>
+                <section className={Styles.first_section}>
+                    <SectionTitle />
+                    <SectionPhoto />
+                </section>
+                <SectionAboutMe />
             </main>
         </body>
     )
